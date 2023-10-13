224fbff8a9b345f8c39d85d2d70058ea1f229dab
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-13 15:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+ function onDOMLoad() {
+            // Write "DOM load success" to the document
+            document.write("DOM load success");
+
+            // Clear the body content
+            document.body.innerHTML = "";
+        }
+
+        // Add an event listener for DOMContentLoaded
+        document.addEventListener("DOMContentLoaded", onDOMLoad);
