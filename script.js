b495c9310b3e4a2f924df175c11a3832f4414893
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-13 15:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
             // Write "DOM load success" to the document
             document.write("DOM load success");
 
-            // Clear the body content
-            document.body.innerHTML = "";
+            // // Clear the body content
+            // document.body.innerHTML = "";
         }
 
         // Add an event listener for DOMContentLoaded
